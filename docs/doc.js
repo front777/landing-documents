7ccf3013028976bd0ad78aa6cbc00b6f780df295
front_magistr <57410622+front777@users.noreply.github.com>
2022-06-06 00:13:12 +0300

    Add files via upload

---
+++ docs/doc.js
@@ -0,0 +1,77 @@
+$("#cont1-act").click(
+    
+
+    function(){
+        $("#cont1").css("display", "block") 
+        $("#cont1-act").css("background", "white")
+        $("#cont4-act").css("background", "none")
+        $("#cont4").css("display", "none") 
+        $("#cont3-act").css("background", "none")
+        $("#cont3").css("display", "none") 
+        $("#cont2-act").css("background", "none")
+        $("#cont2").css("display", "none") 
+        $("#cont5-act").css("background", "none")
+        $("#cont5").css("display", "none") 
+       }
+)
+$("#cont2-act").click(
+    function(){
+        $("#cont2-act").css("background", "white")
+        $("#cont2").css("display", "block") 
+        $("#cont4-act").css("background", "none")
+        $("#cont4").css("display", "none") 
+        $("#cont3-act").css("background", "none")
+        $("#cont3").css("display", "none") 
+        $("#cont5-act").css("background", "none")
+        $("#cont5").css("display", "none") 
+        $("#cont1-act").css("background", "none")
+        $("#cont1").css("display", "none") 
+    },
+    
+)
+$("#cont3-act").click(
+    function(){
+        $("#cont3-act").css("background", "white")
+        $("#cont3").css("display", "block") 
+        $("#cont4-act").css("background", "none")
+        $("#cont4").css("display", "none") 
+        $("#cont5-act").css("background", "none")
+        $("#cont5").css("display", "none") 
+        $("#cont2-act").css("background", "none")
+        $("#cont2").css("display", "none") 
+        $("#cont1-act").css("background", "none")
+        $("#cont1").css("display", "none") 
+    },
+    
+)
+$("#cont4-act").click(
+    function(){
+        $("#cont4-act").css("background", "white")
+        $("#cont4").css("display", "block") 
+        $("#cont5-act").css("background", "none")
+        $("#cont5").css("display", "none") 
+        $("#cont3-act").css("background", "none")
+        $("#cont3").css("display", "none") 
+        $("#cont2-act").css("background", "none")
+        $("#cont2").css("display", "none") 
+        $("#cont1-act").css("background", "none")
+        $("#cont1").css("display", "none") 
+    },
+    
+)
+$("#cont5-act").click(
+    function(){
+        $("#cont5-act").css("background", "white")
+        $("#cont5").css("display", "block") 
+        $("#cont4-act").css("background", "none")
+        $("#cont4").css("display", "none") 
+        $("#cont3-act").css("background", "none")
+        $("#cont3").css("display", "none") 
+        $("#cont2-act").css("background", "none")
+        $("#cont2").css("display", "none") 
+        $("#cont1-act").css("background", "none")
+        $("#cont1").css("display", "none") 
+        
+    },
+    
+)
